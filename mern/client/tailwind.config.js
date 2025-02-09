57c0d3987117c6b1c636daa0427c357805348059
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2025-02-09 16:26:40 +0100

    3

--- mern/client/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,html}"], // Paths to files where Tailwind classes are used
-  theme: {
-    extend: {}, // Customize the default Tailwind styles
-  },
-  plugins: [],
-};
